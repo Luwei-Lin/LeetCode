54bea54e9f2569a805dc7c08dccbc11bd77d405f
Luwei-Lin <luwei2@ualberta.ca>
2022-04-06 15:16:54 -0600

    reorder

--- LC/LC0031_next_permutation.py
+++ LC/LC0031_next_permutation.py
@@ -20,25 +20,42 @@ class Solution:
                 break
         #swap
         if found and not ascending:
-            temp = nums[j]
-            nums[j] = nums[i - 1]
-            nums[i - 1] = temp
-        
-            #reverse
             
+            nums[j], nums[i - 1] = nums[i - 1], nums[j]
+            #reverse
             l = i
             r = len(nums) - 1
             while (l < r):
-                temp = nums[l]
-                nums[l] = nums[r]
-                nums[r] = temp
+                nums[l], nums[r] = nums[r], nums[l]
                 l += 1
                 r -= 1
         else:
             nums.reverse()
+        
+
+    def method2(self, nums):
+        i = len(nums) - 1
+        k = i
+        while (i > 0 and nums[i-1] >= nums[i]):
+            i -= 1
+
+        j = i 
+        while j < k:
+            nums[j], nums[k] = nums[k], nums[j]
+            j += 1
+            k -= 1#move the middle position of subarray
+
+        if (i > 0) :
+            k = i - 1
+            while (nums[k] <= nums[i]):
+                k += 1# find the subarray least one bigger than i
+            nums[i], nums[k] = nums[k], nums[i]
+
+
 def main():
     nums = [1,5,8,4,7,6,5,3,1]
     #nums = [1, 2, 3]
-    Solution().nextPermutation(nums)
+    #Solution().nextPermutation(nums)
+    Solution().method2(nums)
     print(nums)
 main()
