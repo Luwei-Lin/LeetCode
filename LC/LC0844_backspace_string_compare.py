0694e183b2181334c69b75b393caefe51e9fdf1d
Luwei-Lin <luwei2@ualberta.ca>
2022-04-12 17:44:25 -0600

    844

--- LC/LC0844_backspace_string_compare.py
+++ LC/LC0844_backspace_string_compare.py
@@ -1,13 +1,36 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        vs = list[s]
-        vt = list[t]
-        #check the 
-        
+        vs = []
+        vt = []
+        #check the '#'
+        for c in s:
+            if c != '#' :
+                vs.append(c)
+            elif (c == '#' and len(vs) != 0):
+                vs.pop()
+            else:
+                continue
+        for c in t:
+            if c != '#' :
+                vt.append(c)
+            elif (c == '#' and len(vt) != 0):
+                vt.pop()
+            else:
+                continue
+                
+        vs_l = len(vs)
+        vt_l = len(vt)
+        if (vs_l != vt_l):
+            return False
+        else:
+            for i in range(vs_l):
+                if vs[i] != vt[i]:
+                    return False
         return True
         
-        
 def main():
-    s = "ab#c"
-    t = "ad#c"
+    s = "a##c"
+    t = "#a#c"
+    
+    print(Solution().backspaceCompare(s, t))
 main()
