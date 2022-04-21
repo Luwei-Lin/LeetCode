423f8e5f9c513a4ba97fd55d420a84df75bff38b
Luwei-Lin <luwei2@ualberta.ca>
2022-04-21 11:21:03 -0600

    leetcode

---
+++ LC/LC1586_binary_search_tree_iterator2.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <stack>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+void level(TreeNode *root){
+    if (root == nullptr) return;
+    queue<TreeNode*> q;
+    q.push(root);
+    while (!q.empty()){
+        cout << q.front()->val << " ";
+        if (q.front()->left != NULL) q.push(q.front()->left);
+        if (q.front()->right != NULL) q.push(q.front()->right);
+        q.pop();
+    }
+    cout << endl;
+}
+
+TreeNode* createTree(string A[], TreeNode *root, int i, int size){
+    if (i < size){
+        if (A[i] == "null") return nullptr;
+        TreeNode *temp = new TreeNode(stoi(A[i]));
+        root = temp;
+        root->left = createTree(A, root->left, 2 * i + 1, size);
+        root->right = createTree(A, root->right, 2 * i + 2, size);
+    }
+    return root;
+}
+class BSTIterator {
+private:
+    vector<int> vt;
+    int cur = -1;
+    int res = -1;
+public:
+    void inorderVT(TreeNode* root){
+        if(!root) return;
+        inorderVT(root->left);
+        vt.push_back(root->val);
+        inorderVT(root->right);
+    }
+    BSTIterator(TreeNode* root) {
+        inorderVT(root);
+    }
+    
+    int next() {
+        if(hasNext()){
+            res = vt[++cur];
+        }
+        return res;
+    }
+    
+    bool hasNext() {
+        return (cur + 1) < (int)vt.size();
+    }
+
+    bool hasPrev() {
+        //if (cur == vt.size() - 1 && cur != 0) 
+        return (cur - 1) >= 0;   
+    }
+    
+    int prev() {
+        if(hasPrev()){
+            res = vt[--cur]; 
+        }
+        return res;
+    }
+};
+
+int main(){
+
+    string A[] = {"7","3","15","null","null","9","20"};
+    TreeNode* root = new TreeNode();
+    int size = sizeof(A)/sizeof(A[0]);
+    root = createTree(A, root, 0, size);
+    cout << "original: "<< endl;
+    level(root);
+
+    BSTIterator* obj = new BSTIterator(root);
+    int param_1 = obj->next();
+    cout << "next: " << param_1 <<endl;
+    //bool param_2 = obj->hasNext();
+    //cout <<"Next: "<< param_2 <<endl;
+    int param_3 = obj->next();
+    cout << "next: " << param_3 <<endl;
+    
+
+    cout << "prev: "<< obj->prev()<<endl;
+    cout << "next: "<< obj->next()<<endl;
+    cout << "hasNext: "<< obj->hasNext()<<endl;
+    cout << "next: "<< obj->next()<<endl;
+    cout << "next: "<< obj->next()<<endl;
+    cout << "next: "<< obj->next()<<endl;
+    cout << "hasNext: "<< obj->hasNext()<<endl;
+    cout << "hasPrev: "<< obj->hasPrev()<<endl;
+    cout << "prev: "<< obj->prev()<<endl;
+    cout << "prev: "<< obj->prev()<<endl;
+
+    return 0;
+}
