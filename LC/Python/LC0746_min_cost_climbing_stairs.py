b080fafc28d1ebc875486131b10a0fe8f65dad29
Luwei-Lin <luwei2@ualberta.ca>
2022-07-17 18:45:30 -0500

    DP min cost climbing stages

---
+++ LC/Python/LC0746_min_cost_climbing_stairs.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        
+        return 0
+    
+    
+    
+    
+def main():
+    s = Solution()
+    print()
+main()
